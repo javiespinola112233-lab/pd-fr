4bdbae3e4d83029d9f1838fa74d1dedc3126c883
javiespinola112233-lab <javiespinola112233@gmail.com>
2026-01-01 11:46:32 -0300

    config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const API_CONFIG = {
     // URL del backend (servidor Flask en la Acer)
     // IMPORTANTE: Cambia esta URL por la de ngrok cuando ejecutes start_server_online.bat
-    BASE_URL: 'http://separative-tenantable-shan.ngrok-free.dev',
+    BASE_URL: ' https://separative-tenantable-shan.ngrok-free.dev',
 
     // Para acceso remoto, usa la URL de ngrok:
     // BASE_URL: 'https://your-ngrok-url.ngrok.io',
